81c57a6f0ff825d97fb6a61d18f3c5bf2860e179
Petr Pokorný <petr@petrpokorny.cz>
2015-03-11 22:33:07 +0100

    Introducing first version of application model.

---
+++ src/AppBundle/Entity/Rule.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * This file is part of the transcriptor project.
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Rule entity
+ *
+ * @package AppBundle\Entity
+ * @author  Petr Pokorný <petr@petrpokorny.cz>
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="rule")
+ */
+class Rule {
+
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=5)
+     */
+    private $sourceLanguage;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=5)
+     */
+    private $targetLanguage;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pattern;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $replacement;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSourceLanguage()
+    {
+        return $this->sourceLanguage;
+    }
+
+    /**
+     * @param string $sourceLanguage
+     */
+    public function setSourceLanguage($sourceLanguage)
+    {
+        $this->sourceLanguage = $sourceLanguage;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTargetLanguage()
+    {
+        return $this->targetLanguage;
+    }
+
+    /**
+     * @param string $targetLanguage
+     */
+    public function setTargetLanguage($targetLanguage)
+    {
+        $this->targetLanguage = $targetLanguage;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPattern()
+    {
+        return $this->pattern;
+    }
+
+    /**
+     * @param string $pattern
+     */
+    public function setPattern($pattern)
+    {
+        $this->pattern = $pattern;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReplacement()
+    {
+        return $this->replacement;
+    }
+
+    /**
+     * @param string $replacement
+     */
+    public function setReplacement($replacement)
+    {
+        $this->replacement = $replacement;
+    }
+
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * This file is part of the transcriptor project.
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User entity
+ *
+ * @package AppBundle\Entity
+ * @author  Petr Pokorný <petr@petrpokorny.cz>
+ *
+ * @ORM\Entity
+ * @ORM\Table
+ */
+class User {
+
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+}
